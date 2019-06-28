19cab49544a0cc5c5d006fc31556e43b951b534b
Claudio Souza <c.souza0804@gmail.com>
2019-06-27 22:05:44 -0300

    Stop on class 87 and Adding FormsModule

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() itemWasSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('First test recipe', 'Lorem Ipsum', 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-12.jpg'),
-    new Recipe('Second test recipe', 'Lorem Ipsum', 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-12.jpg')
+    new Recipe('First test recipe', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ", 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-12.jpg'),
+    new Recipe('Second test recipe', "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'", 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-12.jpg'),
+    new Recipe('Third test recipe', "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.", 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-12.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onItemSelected (itemSelected: Recipe) {
+    this.itemWasSelected.emit(itemSelected);
   }
 
 }
